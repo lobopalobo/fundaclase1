51d51a1a1fc0a56d531fcf74b137f85c17d7f2d8
lobopalobo <>
2021-09-27 13:02:38 +0000

    hallar area de un rombo

--- estsecuencial/estsecuencial.py
+++ estsecuencial/estsecuencial.py
@@ -1,4 +1,8 @@
-#dato de entrada
-DMx=int(imput("ingrese diagonal mayor"))
-dmx=int(imput("ingrese diagonal menor"))
-#proceso
+#Datos de entrada
+print("Hallar el area de un rombo")
+DM=int(input("Ingrese diagonal mayor:"))
+dm=int(input("Ingrese diagonal menor:"))
+#Proceso
+area=(DM*dm)/2
+#Datos de salida
+print("El area del rombo es:", area , "mt2")
